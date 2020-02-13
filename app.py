c1a4bbaa5e128b1ee25e40587ecf4b40c7b75f82
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-02-12 21:16:52 -0600

    [ADD] Login page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from odoo import server
-from config import Config      
+from config import Config    
+from forms import LoginForm  
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,7 +12,17 @@ app.config.from_object(Config)
 def index():
     test = server().connection()
     qs = request.args
-    return render_template('index.html', user = test, q=qs)
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
