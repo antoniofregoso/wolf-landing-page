6cc3d27473fa4afc356d84da00bca5b10dfea85f
antoniofregoso <antoniofregoso@antoniofregoso-desktop>
2020-02-28 17:50:56 -0600

    [FIX] Mobile First

--- models/odoo.py
+++ models/odoo.py
@@ -18,6 +18,8 @@ class server:
         username  = odoo['user']
         password = odoo['pwd']
         return 'd'
+    
+    
         
         
         
--- static/js/wolf.js
+++ static/js/wolf.js
@@ -21,7 +21,97 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	});
 });
 
-
 function wolfNav() {
-	$(".nav-wolf ").removeClass('base-wolf').addClass('base-wolf-over animated slideInLeft');
+	if($('#nav-wolf').hasClass('dock-wolf')){
+		$('#nav-wolf').addClass('over-wolf');
+		$('.nav-wolf').animate({
+			width: '190px'
+		}, 1000);
+		$('#nav-wolf').removeClass('dock-wolf');		
+	} else {
+		$('#nav-wolf').removeClass('over-wolf');
+		$('.nav-wolf').animate({
+			width: '70px'
+		}, 500);
+		$('#nav-wolf').addClass('dock-wolf');
 	}
+	};
+	
+	(function($) {
+		  'use strict';
+
+		  $.fn.launchBtn = function(options) {
+		    var mainBtn, panel, clicks, settings, launchPanelAnim, closePanelAnim, openPanel, boxClick;
+
+		    mainBtn = $(".st-event-button-main");
+		    panel = $(".st-event-panel");
+		    clicks = 0;
+
+		    //default settings
+		    settings = $.extend({
+		      openDuration: 600,
+		      closeDuration: 200,
+		      rotate: true
+		    }, options);
+
+		    //Open panel animation
+		    launchPanelAnim = function() {
+		      panel.animate({
+		        opacity: "toggle",
+		        height: "toggle"
+		      }, settings.openDuration);
+		    };
+
+		    //Close panel animation
+		    closePanelAnim = function() {
+		      panel.animate({
+		        opacity: "hide",
+		        height: "hide"
+		      }, settings.closeDuration);
+		    };
+
+		    //Open panel and rotate icon
+		    openPanel = function(e) {
+		      if (clicks === 0) {
+		        if (settings.rotate) {
+		          $(this).removeClass('rotateBackward').toggleClass('rotateForward');
+		        }
+
+		        launchPanelAnim();
+		        clicks++;
+		      } else {
+		        if (settings.rotate) {
+		          $(this).removeClass('rotateForward').toggleClass('rotateBackward');
+		        }
+
+		        closePanelAnim();
+		        clicks--;
+		      }
+		      e.preventDefault();
+		      return false;
+		    };
+
+		    //Allow clicking in panel
+		    boxClick = function(e) {
+		      e.stopPropagation();
+		    };
+
+		    //Main button click
+		    mainBtn.on('click', openPanel);
+
+		    //Prevent closing panel when clicking inside
+		    panel.click(boxClick);
+
+		    //Click away closes panel when clicked in document
+		    $(document).click(function() {
+		      closePanelAnim();
+		      if (clicks === 1) {
+		        mainBtn.removeClass('rotateForward').toggleClass('rotateBackward');
+		      }
+		      clicks = 0;
+		    });
+		  };
+		}(jQuery));
+
+
+		
