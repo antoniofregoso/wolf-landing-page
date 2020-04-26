266058399a8623b2ee17c531ae60403464475716
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-04-26 16:39:29 -0500

    [FIX]

--- wolf/__init__.py
+++ wolf/__init__.py
@@ -5,7 +5,9 @@ from wolf.models.forms  import LeadForm
 
 app = Flask(__name__)
 #app.config.from_object(Config)
-
+import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = 'truurucc845rjdue894jdd4546'
 
 @app.route('/')
 @app.route('/index')
